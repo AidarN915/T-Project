66c5d270568a9adbcff798054a9e41cb8c3e3d6f
BoBr <nurdinov200202@mail.ru>
2025-11-25 19:15:13 +0600

    @RequestParam(inn) String inn

--- src/main/java/Tproject/controller/AuthController.java
+++ src/main/java/Tproject/controller/AuthController.java
@@ -15,10 +15,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import Tproject.repository.UserRepository;
 import Tproject.service.UserDetailsServiceImpl;
 import Tproject.util.JwtUtil;
@@ -80,7 +77,7 @@ public class AuthController {
         return ResponseEntity.ok("Ок");
     }
     @PostMapping("/refresh")
-    public ResponseEntity<JwtDto> refresh(@RequestBody String refreshToken){
+    public ResponseEntity<JwtDto> refresh(@RequestParam("inn") String refreshToken){
         JwtDto dto = authService.refresh(refreshToken);
         return ResponseEntity.ok(dto);
     }
