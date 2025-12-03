77806249795560f546a8bb6b6296a9a91456275c
BoBr <nurdinov200202@mail.ru>
2025-12-03 16:50:08 +0600

    websocket chat test2

--- src/main/java/Tproject/repository/ChatMessageRepository.java
+++ src/main/java/Tproject/repository/ChatMessageRepository.java
@@ -3,9 +3,9 @@ package Tproject.repository;
 import Tproject.model.ChatMessage;
 import Tproject.model.ChatRoom;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.awt.print.Pageable;
 import java.util.List;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage,Long> {
