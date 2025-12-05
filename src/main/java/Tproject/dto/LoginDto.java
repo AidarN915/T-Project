5a670503e0b42fbb465d0622d4333eaf38757769
BoBr <nurdinov200202@mail.ru>
2025-12-05 12:00:17 +0600

    login id

--- src/main/java/Tproject/dto/LoginDto.java
+++ src/main/java/Tproject/dto/LoginDto.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class LoginDto {
-    private Long userId;
+    private Long id;
     private String username;
     private String role;
     private String token;
