e4fb8d032aec7d4b1111efe1bac1526f6b8fe47c
BoBr <nurdinov200202@mail.ru>
2025-12-07 21:19:40 +0600

    chat users lazy load fix

--- src/main/java/Tproject/repository/ChatRoomRepository.java
+++ src/main/java/Tproject/repository/ChatRoomRepository.java
@@ -3,12 +3,17 @@ package Tproject.repository;
 import Tproject.model.ChatRoom;
 import Tproject.model.Task;
 import Tproject.model.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom,Long> {
+    @Override
+    @EntityGraph(attributePaths = {"users"})
+    Optional<ChatRoom> findById(Long id);
+
     public List<ChatRoom> findByUsers(User user);
     public Optional<ChatRoom> findByTask(Task task);
     public Optional<ChatRoom> findByChatRoomKey(String chatRoomKey);
